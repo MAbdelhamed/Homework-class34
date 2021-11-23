b62fe20326c1e3c5c3540663dbc0c0121b983b46
Mohamed Abdelhamed <56353109+MAbdelhamed@users.noreply.github.com>
2021-11-23 17:04:10 +0100

    exercise 1 updated v1.1

--- 3-UsingAPIs/Week1/homework/ex1-johnWho.js
+++ 3-UsingAPIs/Week1/homework/ex1-johnWho.js
@@ -11,18 +11,22 @@ Rewrite this function, but replace the callback syntax with the Promise syntax:
 
 const getAnonName = (firstName) => {
   return new Promise((resolve, reject) => {
-    if (!firstName) {
-      reject(new Error("You didn't pass in a first name!"));
-      return;
-    }
+    setTimeout(() => {
+      if (!firstName) {
+        reject(new Error("You didn't pass in a first name!"));
+        return;
+      }
 
-    const fullName = `${firstName} Doe`;
-    resolve(fullName);
-    return;
+      const fullName = `${firstName} Doe`;
+      resolve(fullName);
+      return;
+    }, 1000);
   });
 };
 
-getAnonName('John');
+getAnonName('John')
+  .then((fullName) => console.log(fullName))
+  .catch((error) => console.log(error));
   
 
 
