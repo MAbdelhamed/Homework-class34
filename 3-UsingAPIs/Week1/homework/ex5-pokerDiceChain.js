4dfe07ab53e88880311403b6a508dd2cb7891970
Mohamed Abdelhamed <56353109+MAbdelhamed@users.noreply.github.com>
2021-11-23 15:21:37 +0100

    exercise 5 solved

--- 3-UsingAPIs/Week1/homework/ex5-pokerDiceChain.js
+++ 3-UsingAPIs/Week1/homework/ex5-pokerDiceChain.js
@@ -17,11 +17,22 @@ const rollDice = require('../../helpers/pokerDiceRoller');
 function rollTheDices() {
   const results = [];
 
-  // TODO: expand the chain to include five dices
   return rollDice(1)
     .then((value) => {
       results.push(value);
       return rollDice(2);
+    })
+    .then((value) => {
+      results.push(value);
+      return rollDice(3);
+    })
+    .then((value) => {
+      results.push(value);
+      return rollDice(4);
+    })
+    .then((value) => {
+      results.push(value);
+      return rollDice(5);
     })
     .then((value) => {
       results.push(value);
@@ -32,6 +43,8 @@ function rollTheDices() {
 rollTheDices()
   .then((results) => console.log('Resolved!', results))
   .catch((error) => console.log('Rejected!', error.message));
+
+  
 
 // ! Do not change or remove the code below
 module.exports = rollTheDices;
